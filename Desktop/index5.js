f536c16364f82a628dc7f8d02cafd507c3af3cb7
blazers85 <ahmednw9@gmail.com>
2018-07-02 18:26:00 -0700

    hw

---
+++ Desktop/index5.js
@@ -0,0 +1,21 @@
+function sum(){
+ var sum =0;
+ for(var i=0;i<arguments.length;i++){
+    sum += arguments[i];
+ }
+  return sum;
+}
+sum(0,1,2); 
+sum(0,2,3,4); 
+
+
+
+function sum(){
+ var sum =0;
+ for(var i=0;i<arguments.length;i++){
+    sum += arguments[i];
+ }
+  return sum;
+}
+sum(2,2); 
+sum(2,19,2); 
